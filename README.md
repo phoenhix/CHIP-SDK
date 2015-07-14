427f89c1b5665e9965f761fe092bb0cdd1d92ae6
Alex Kaplan <kaplan2539@gmail.com>
2015-07-13 19:09:50 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# CHIP-SDK
+Everything needed to develop software for C.H.I.P.
+
+While it is possible to install the SDK natively on your computer, currently the only supported way is to run it from a virtual machine.
+
+## System Requirements
+1 GB RAM
+
+## Getting started with the Vagrant image
+
+
+### Installation
+1. Install VirtualBox
+2. Install VirtualBox Extension Pack (for the host)
+3. Operating system specific stuff:
+   - If you are on Windows, you need to add the VirtualBox installation directory to your PATH.
+   - In case of a Ubuntu host: add your user to the vboxusers group!
+4. Install Git
+4. Git clone https://github.com/NextThingCo/CHIP-SDK
+
+#### Start up the Virtual Machine
+In a shell on the host do:
+ - `cd chip_sdk/vagrant`
+ - `vagrant up` # starts vm
+
+#### Place you private ssh key
+These steps won't be necessary after the repositories have been opened up:
+ - create a ssh key pair and link the public key to your github account
+ - copy the private ssh key (id_rsa) to chip_sdk/vagrant
+
+#### Install software and build the flash image for CHIP
+In a shell on the host do:
+ - `vagrant ssh` # log into vm
+
+Logged in to the virtual machine do:
+ - `/vagrant/install.sh`
+
+#### Shutdown the Virtual machine
+In a shell on the host do:
+  - `vagrant halt` # stops vm
+
+#### Troubleshooting
+In case you run into trouble because the kernel in the VM was updated and the shared valgrant folder can no longer be mounted, update the guest additions by typing the following in the chip_sdk/vagrant directory on the host:
+  - `vagrant plugin install vagrant-vbguest`
+
+Also look at [this blog post](http://kvz.io/blog/2013/01/16/vagrant-tip-keep-virtualbox-guest-additions-in-sync/)
---
+++ vagrant/install.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+VERSION=0.0.1
+
+BUILDROOT_REPO=git@github.com:NextThingCo/CHIP-buildroot.git
+TOOLS_REPO=git@github.com:NextThingCo/CHIP-tools.git
+##BUILDROOT_REPO=git@tau.free-electrons.com:nextthing/buildroot.git
+BUILDROOT_BRANCH=ak/chip-nand
+
+TARGET_DIR=~/chip_sdk
+
+#----------------------------------------------------------------------------#
+
+echo -e "\n C.H.I.P SDK installer v${VERSION}\n"
+
+if ! echo $HOSTNAME |grep -q vagrant; then
+   echo -e "Not running inside vagrant currently not supported - sorry\n"
+   exit 1
+else
+
+  if [ ! -e ~/.ssh ]; then mkdir ~/.ssh; fi
+  if [ ! -f ~/.ssh/id_rsa ]; then
+    if [ -f /vagrant/id_rsa ]; then
+      cp /vagrant/id_rsa ~/.ssh
+    fi
+  fi
+  chmod 0600 ~/.ssh/id_rsa
+
+  echo "StrictHostKeyChecking no" >~/.ssh/config  
+
+  ssh-agent >/tmp/ssh-agent.sh
+  source /tmp/ssh-agent.sh
+  ssh-add
+
+fi
+
+echo -e "\n Installing packages"
+sudo apt-get -y install \
+ build-essential \
+ git \
+ cmake \
+ unzip \
+ device-tree-compiler \
+ libncurses-dev \
+ cu \
+ linux-image-extra-virtual
+
+sudo add vagrant dialout
+
+if [ ! -d ${TARGET_DIR} ]; then
+  mkdir ${TARGET_DIR}
+fi
+cd ${TARGET_DIR}
+
+echo -e "\n Cloning & building ${BUILDROOT_REPO}"
+LOCAL_REPO_NAME=${BUILDROOT_REPO##*/}
+LOCAL_REPO_NAME=${LOCAL_REPO_NAME##CHIP-}
+LOCAL_REPO_NAME=${LOCAL_REPO_NAME%.git}
+git clone ${BUILDROOT_REPO} ${LOCAL_REPO_NAME}
+pushd ${LOCAL_REPO_NAME}
+git checkout ${BUILDROOT_BRANCH}
+git pull
+make chip_alex_defconfig
+make
+popd
+
+echo -e "\n Cloning & building ${TOOLS_REPO}"
+LOCAL_REPO_NAME=${TOOLS_REPO##*/}
+LOCAL_REPO_NAME=${LOCAL_REPO_NAME##CHIP-}
+LOCAL_REPO_NAME=${LOCAL_REPO_NAME%.git}
+git clone ${TOOLS_REPO} ${LOCAL_REPO_NAME}
+pushd ${LOCAL_REPO_NAME}
+git checkout ${TOOLS_BRANCH}
+git pull
+popd
